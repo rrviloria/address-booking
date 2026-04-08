2d873d64d21f12c07045bd26f6b50d65be92c99b
Raymund Viloria <raymund.viloria@gmail.com>
2026-04-08 22:40:48 +0800

    chore: error logging and response

---
+++ app/core/loggger.py
@@ -0,0 +1,27 @@
+import logging
+from typing import Callable
+from fastapi import Request, Response
+from fastapi.routing import APIRoute
+
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
+class RouteLogger(APIRoute):
+    def get_route_handler(self, *args, **kwargs) -> Callable:
+        original_route_handler = super().get_route_handler(*args, **kwargs)
+
+        async def custom_route_handler(request: Request, *args, **kwargs) -> Response:
+            method = request.method
+            if method in ["POST", "PATCH"]:
+                body = await request.body()
+                logger.info(f"Request body: {body.decode()}")
+
+            response: Response = await original_route_handler(request)
+
+            if method in ["POST", "PATCH"]:
+                logger.info(f"Response body: {response.body.decode()}")
+            return response
+
+        return custom_route_handler
