c9df840c1702e6f345f6a9fe8f72b34451493337
Raymund <raymund.viloria@gmail.com>
2026-04-09 15:04:42 +0800

    fix: make long and lat float

--- app/booking/models.py
+++ app/booking/models.py
@@ -1,12 +1,19 @@
 from decimal import Decimal
 from sqlmodel import Field, SQLModel, Relationship
+from pydantic import BaseModel
 from ..oauth.models import User
 
 
 class Address(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    longitude: Decimal = Field(max_digits=9, decimal_places=6)
-    latitude: Decimal = Field(max_digits=9, decimal_places=6)
+    longitude: float
+    latitude: float
     name: str = Field(default="")
     user_id: int | None = Field(default=None, foreign_key="user.id")
     user: User = Relationship(back_populates="address")
+
+
+class Location(BaseModel):
+    longitude: float
+    latitude: float
+    distance: int
